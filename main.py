f7abc6bb09337c22133ee2c39931c3484b6461e3
Samer-Ismael <samerismaeelbat2@hotmail.com>
2024-05-06 05:41:11 +0200

    Adding a server to show the cards as endpoint

--- main.py
+++ main.py
@@ -131,19 +131,8 @@ class CardDetector:
         similarity_index = compare_ssim(gray1, gray2)
         return similarity_index
 
-    def print_remaining_cards(self):
-        standard_deck = {
-            'Ace of Spades', 'Two of Spades', 'Three of Spades', 'Four of Spades', 'Five of Spades', 'Six of Spades', 'Seven of Spades', 'Eight of Spades', 'Nine of Spades', 'Ten of Spades', 'Jack of Spades', 'Queen of Spades', 'King of Spades',
-            'Ace of Hearts', 'Two of Hearts', 'Three of Hearts', 'Four of Hearts', 'Five of Hearts', 'Six of Hearts', 'Seven of Hearts', 'Eight of Hearts', 'Nine of Hearts', 'Ten of Hearts', 'Jack of Hearts', 'Queen of Hearts', 'King of Hearts',
-            'Ace of Clubs', 'Two of Clubs', 'Three of Clubs', 'Four of Clubs', 'Five of Clubs', 'Six of Clubs', 'Seven of Clubs', 'Eight of Clubs', 'Nine of Clubs', 'Ten of Clubs', 'Jack of Clubs', 'Queen of Clubs', 'King of Clubs',
-            'Ace of Diamonds', 'Two of Diamonds', 'Three of Diamonds', 'Four of Diamonds', 'Five of Diamonds', 'Six of Diamonds', 'Seven of Diamonds', 'Eight of Diamonds', 'Nine of Diamonds', 'Ten of Diamonds', 'Jack of Diamonds', 'Queen of Diamonds', 'King of Diamonds'
-        }
-        #remaining_cards = standard_deck - self.detected_cards
-        #print("Remaining cards:")
-        #for card in remaining_cards:
-            #print(card)
-        
-        print(self.detected_cards)
+    def get_detected_cards(self):
+        return list(self.detected_cards)
 
     def main(self):
         while True:
@@ -159,7 +148,9 @@ class CardDetector:
             self.print_remaining_cards()
             
             time.sleep(1)
-            
+
+    def print_remaining_cards(self):
+        print(self.detected_cards)
 
 if __name__ == "__main__":
     card_detector = CardDetector(card_folder="cards")
---
+++ service.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify
+from main import CardDetector
+import time
+import threading
+
+app = Flask(__name__)
+
+# Initialize CardDetector instance
+card_detector = CardDetector(card_folder="cards")
+
+@app.route('/cards')
+def get_detected_cards():
+    detected_cards = card_detector.get_detected_cards()
+    return jsonify(detected_cards)
+
+def run_card_detection():
+    while True:
+        card_detector.get_pic_and_save()
+        time.sleep(1)
+
+if __name__ == '__main__':
+    # Start a separate thread for card detection
+    threading.Thread(target=run_card_detection, daemon=True).start()
+    app.run(debug=True)
